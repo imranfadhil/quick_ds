e8495dc6a92a6b21bf4f45191453d6c889c788df
imranfadhil <imranfadhil@gmail.com>
2026-01-06 13:00:59 +0800

    integrating kedro and zenml with default spaceflight example

---
+++ main.py
@@ -0,0 +1,9 @@
+import sys
+from importlib import import_module
+
+
+if __name__ == "__main__":
+    main = import_module("src.quick_ds.cli").cli
+
+    if len(sys.argv) > 1:
+        main(sys.argv[1:])
